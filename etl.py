e9358a07f76825947adcea29330dde5f5d35dad4
Matheus <matheusvirgolino0@gmail.com>
2025-04-28 21:02:44 -0300

    feat: add funcao carregar_dados

--- etl.py
+++ etl.py
@@ -13,6 +13,16 @@ def calcular_kpi_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
 
+def carregar_dados(df: pd.DataFrame, format_saida: list):
+    """
+    Parametros: "csv" e/ou "parquet" em uma lista
+    """
+    for i in format_saida:
+        if i == "csv":
+            df.to_csv("dados.csv")
+        if i == "parquet":
+            df.to_parquet("dados.parquet")
+
 if __name__ == "__main__":
     pasta = "data"
     print(extrair_dados(path=pasta))
