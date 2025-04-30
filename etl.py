f9f7a2b699263302d745154d46796a7223a54adc
Matheus <matheusvirgolino0@gmail.com>
2025-04-29 21:39:49 -0300

    chore: code style + testing

--- etl.py
+++ etl.py
@@ -4,25 +4,29 @@ import glob
 
 
 def extrair_dados(path: str) -> pd.DataFrame:
-        arquivos_json = glob.glob(os.path.join(path, '*.json'))
-        df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
-        df_total = pd.concat(df_list, ignore_index=True)
-        return df_total
+    arquivos_json = glob.glob(os.path.join(path, '*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
+
 
 def calcular_kpi_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
-    df["Total"] = df["Quantidade"] * df["Venda"]
+    df['Total'] = df['Quantidade'] * df['Venda']
     return df
+
 
 def carregar_dados(df: pd.DataFrame, format_saida: list):
     """
     Parametros: "csv" e/ou "parquet" em uma lista
     """
     for i in format_saida:
-        if i == "csv":
-            df.to_csv("dados.csv")
-        if i == "parquet":
-            df.to_parquet("dados.parquet")
+        if i == 'csv':
+            df.to_csv('dados.csv')
+        if i == 'parquet':
+            df.to_parquet('dados.parquet')
 
-if __name__ == "__main__":
-    pasta = "data"
-    print(extrair_dados(path=pasta))
+
+if __name__ == '__main__':
+    pasta = 'data'
+    dados = extrair_dados(path=pasta)
+    carregar_dados(dados, ["csv", "parquet"])
